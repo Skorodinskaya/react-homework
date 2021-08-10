12aec118368dcf8ebeba63e712c4a8815a5aff44
nast <skorodinskaya01@gmail.com>
2021-08-10 22:53:59 +0200

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import './App.css'
+import Simpson from "./components/simpson/Simpson";
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Simpson name ='Bart' surname = 'Simpson' age = '10' info = 'Бартоломю ДжоДжо «Барт» Сімпсон (англ. Bartholomew JoJo «Bart» Simpson) — один із головних героїв мультиплікаційного серіалу Сімпсони. Барт — найстарша дитина Гомера і Мардж Сімпсон. У нього також є дві молодші сестри — Ліса і Меґґі. Барт є втіленням образу бешкетника та посереднього учня у школі. Разом зі своїм батьком Барт є одним із найвідоміших персонажів у цьому серіалі.' image = 'https://upload.wikimedia.org/wikipedia/uk/a/aa/Bart_simpson.p'/>
+
     </div>
   );
 }
-
-export default App;
---
+++ src/components/simpson/Simpson.js
@@ -0,0 +1,10 @@
+export default function Simpson(name, surname, age, info, image) {
+    return (
+        <div>
+            <h2>{name} {surname}. Age - {age}</h2>
+            <p> {info}</p>
+            <img src={image} alt=""/>
+
+        </div>
+    );
+}
