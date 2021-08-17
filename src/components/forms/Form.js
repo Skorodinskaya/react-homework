45936f9d8eb21eccf933e721b35e4c8d2a9f186b
Skorodinskaya <skorodinskaya01@gmail.com>
2021-08-17 19:20:53 +0300

    some changes

--- src/components/forms/Form.js
+++ src/components/forms/Form.js
@@ -20,7 +20,7 @@ export default function Form() {
         <div>
             <form onSubmit={save}>
                 <input type="text" name={'model'} value={formState.model} maxLength={20} onInput={onFormInputChange}/>
-                <input type="text" name={'price'} value={formState.price} min = {0} onInput={onFormInputChange}/>
+                <input type="number" name={'price'} value={formState.price} min = {0} onInput={onFormInputChange}/>
                 <input type="number" name={'year'} value={formState.year} min={1990} max ={2021} onInput={onFormInputChange}/>
                 <input type="submit"/>
             </form>
