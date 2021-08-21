3a5ed38a1675180d7b2391e995c8b3b6e29161e9
Skorodinskaya <skorodinskaya01@gmail.com>
2021-08-22 01:20:45 +0300

    Some changes

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -3,7 +3,7 @@ import {Link} from "react-router-dom";
 export default function Post({item}) {
   return (
     <div>
-        {item.id}. {item.title} - <Link to = {{pathname: '/posts/' + item.id}} >Post details</Link>
+        {item.id}. {item.title} - <Link to = {{pathname: '/posts/' + item.id, state: item}} >Post details</Link>
 
     </div>
   );
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -5,17 +5,17 @@ import UserDetails from "../user/UserDetails";
 import {getUsers} from "../../services/api.service";
 
 export default function Users(props) {
-  const {match: {url}, history} = props;
-  const [users, setUsers] = useState([]);
-  useEffect(()=> {
-    getUsers().then(value => setUsers([...value]))
-  }, [])
-  return (
-    <div>
-      {users.map(value => <User history={history} item={value} key = {value.id}/>)}
-<Route path={`${url}/:id`} render={(props) => {
-  return <UserDetails {...props}/>
-}}/>
-    </div>
-  );
+    const {match: {url}, history} = props;
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        getUsers().then(value => setUsers([...value]))
+    }, [])
+    return (
+        <div>
+            {users.map(value => <User history={history} item={value} key={value.id}/>)}
+            <Route path={`${url}/:id`} render={(props) => {
+                return <UserDetails {...props}/>
+            }}/>
+        </div>
+    );
 }
