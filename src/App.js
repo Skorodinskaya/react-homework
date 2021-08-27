1c1a7ad02adf4ad84ce11e5f9083a63fdf35b6b0
Skorodinskaya <skorodinskaya01@gmail.com>
2021-08-28 01:06:40 +0300

    Some changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
-export default function App() {
-  return (
-    <div>
-    App
+import {useDispatch, useSelector} from "react-redux";
+import {useEffect} from "react";
+import {addUser, getUsers} from "./services/api.service";
 
-    </div>
-  );
+export default function App() {
+    let state = useSelector(state => state);
+    let dispatch = useDispatch()
+    let {users} = state;
+    useEffect(() => {
+        getUsers().then(value => {
+            let action = {type: 'GET_USERS', payload: value}
+            dispatch(action)
+        });
+    }, []);
+    let onSubmit = (e) => {
+        e.preventDefault();
+        let name = e.target.name.value;
+        let user = {name};
+        addUser(user).then(value =>
+            console.log(value))
+        dispatch({type: 'PUSH_USER', payload: value})
+    }
+    // let onClickClearState = () => {
+    //   dispatch({type: 'CLEAR_STORE'})
+    // };
+    // let onClickSetState= () => {
+    //   getUsers().then(value => dispatch({type:'GET_USERS', payload: value}))
+    // };
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input type={'name'} name={'name'}/>
+                <input type={'submit'}/>
+            </form>
+            <hr/>
+            {/*<button onClick={onClickClearState}>Clear users</button>*/}
+            {/*<hr/>*/}
+            {/*<button onClick={onClickSetState}>Set users</button>*/}
+            {/*<hr/>*/}
+            {users.map((value) => <div>{value.name}</div>)}
+        </div>
+    );
 }
